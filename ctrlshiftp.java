d8b7724b2bbbe190876c1f70f3a74531afeb5b4b
aeszala <aeszala@gmail.com>
2025-01-16 12:06:58 -0500

    adding hello file, updating ctrlshiftp

---
+++ ctrlshiftp.java
@@ -0,0 +1,13 @@
+// ctrl shift p
+// f5 to run code
+
+public class ctrlshiftp {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+    void print(String s) {
+        System.out.println(s);
+    }
+
+}
