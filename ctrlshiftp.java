9e4ebdf579de9e356d70dc84ff759223b3da11ac
aeszala <aeszala@gmail.com>
2025-01-16 12:16:38 -0500

    updated print function

--- ctrlshiftp.java
+++ ctrlshiftp.java
@@ -7,10 +7,10 @@
 
 public class ctrlshiftp {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        print("Hello World!");
     }
 
-    void print(String s) {
+    static void print(String s) {
         System.out.println(s);
     }
 
